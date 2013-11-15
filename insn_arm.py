4fddd3e6afb3b20b74d36103c529080118181c8a
Ulrich Hecht <ulrich.hecht@gmail.com>
2013-11-15 23:11:13 +0100

    insn, ssa: add ARM support

---
+++ insn_arm.py
@@ -0,0 +1,115 @@
+# insn_arm.py
+# Copyright (C) 2013 Ulrich Hecht
+
+# This file is part of 6502 Decompiler.
+
+# 6502 Decompiler is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3 as published
+# by the Free Software Foundation.
+
+# 6502 Decompiler is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with 6502 Decompiler.  If not, see <http://www.gnu.org/licenses/>.
+
+from debug import *
+from insn import Symbol
+import struct
+from copy import copy
+
+def trace(self, code, org, addr, ins):
+    opcode = struct.unpack('<I', code[addr-org : addr-org+4])[0]
+    #print(opcode)
+    ins.bytes = [opcode]
+
+    ins.cond = opcode >> 28
+    ins.op = (opcode >> 20) & 0xff
+    
+    link_ins = ins
+    if ins.cond != 0xe and ins.op >> 4 != 0xa:
+      # conditional instruction that is not a branch
+      # insert a fake branch with inverted condition to the nearest insn with a different
+      # condition code
+      debug(TRACE, 4, 'conditional non-branch at', hex(addr))
+
+      ins2 = copy(ins)
+      ins2.comefrom = [ins]
+      # make this insn unconditional
+      ins2.cond = 0xe
+
+      cond_end = addr + 4
+      while struct.unpack('<I', code[cond_end-org : cond_end-org+4])[0] >> 28 == ins.cond:
+        # modify machine code to make subsequent insns with the same condition
+        # unconditional
+        # XXX: yuck; find a way to do this without changing the source text
+        code[cond_end+3] = (code[cond_end+3] & 0xf) | 0xe0
+        cond_end += 4
+
+      ins.artificial_branch = cond_end
+      ins.cond = ins.cond ^ 1
+      ins.op = 0xa0 # Bcc
+      ins.next = [ins2, self.trace(code, org, cond_end, ins)]
+
+      ins = ins2
+    
+    debug(TRACE, 4, "continuing", hex(addr))
+    ins.set_flags = (opcode >> 20) & 1
+    ins.rn = (opcode >> 16) & 0xf
+    ins.rd = (opcode >> 12) & 0xf
+    ins.rs = (opcode >> 8) & 0xf
+    ins.shift_bits = (opcode >> 7) & 0x1f
+    ins.shift = (opcode >> 5) & 0x3
+    ins.shift_rot = (opcode >> 4) & 0x1
+    ins.rm = opcode & 0xf
+    ins.imm8 = opcode & 0xff
+    ins.imm12 = opcode & 0xfff
+    ins.off24 = opcode & 0xffffff
+    if ins.off24 >= 0x800000:
+      ins.off24 -= 0x1000000
+    ins.reglist = opcode & 0xffff
+    
+    debug(TRACE, 4, 'opcode '+hex(opcode), 'op', hex(ins.op), 'cond', hex(ins.cond))
+
+    if ins.op >> 4 == 0xa:
+      daddr = addr + ins.off24 * 4 + 8
+      debug(TRACE, 4, 'B', hex(ins.off24), hex(ins.cond))
+      if ins.cond == 0xe:
+        ins.next = [self.trace(code, org, daddr, ins)]
+      else:
+        ins.next = [self.trace(code, org, addr + 4, ins), self.trace(code, org, daddr, ins)]
+    elif ins.op >> 4 == 0xb:
+      daddr = addr + ins.off24 * 4 + 8
+      debug(TRACE, 4, 'BL', hex(ins.off24))
+      ins.next = [self.trace(code, org, addr + 4, ins), self.trace(code, org, daddr, ins, Symbol(daddr, 'fun'))]
+    elif opcode & 0x0ffffff0 == 0x012fff10 and ins.cond == 0xe:
+      debug(TRACE, 4, 'EOT (BX r' + str(ins.rm) + ')')
+      ins.next = None
+    elif ins.op & 0xe5 == 0x81 and ins.cond == 0xe and (ins.reglist & 0x8000) == 0x8000:
+      debug(TRACE, 4, 'EOT (LDM {...pc}')
+      ins.next = None
+    elif ins.op & 0xe5 == 0x41 and ins.rd == 15:
+      # LDR pc,[...]
+      if ins.rn != 15:
+        debug(TRACE, 4, 'EOT (LDR pc,[...])')
+        if ins.cond != 0xe:
+          ins.next = [self.trace(code, org, addr + 4, ins)]
+        else:
+          ins.next = None
+      else:
+        debug(TRACE, 4, 'LDR pc,[pc...])')
+        daddr_loc = ins.imm12 + addr + 8
+        daddr = struct.unpack('<I', code[daddr_loc:daddr_loc + 4])[0]
+        if ins.cond != 0xe:
+          ins.next = [self.trace(code, org, addr + 4, ins), self.trace(code, org, daddr, ins)]
+        else:
+          ins.next = [self.trace(code, org, daddr, ins)]
+    elif ins.op & 0xfe == 0x1a and ins.rd == 15:
+      debug(TRACE, 4, 'EOT (MOV pc,...)')
+      ins.next = None
+    else:
+      ins.next = [self.trace(code, org, addr + 4, ins)]
+
+    return link_ins
---
+++ ssa_arm.py
@@ -0,0 +1,548 @@
+# ssa_arm.py
+# Copyright (C) 2013 Ulrich Hecht
+
+# This file is part of 6502 Decompiler.
+
+# 6502 Decompiler is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3 as published
+# by the Free Software Foundation.
+
+# 6502 Decompiler is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with 6502 Decompiler.  If not, see <http://www.gnu.org/licenses/>.
+
+from debug import *
+from ssa import SSAStatement, SSADef, IMPURE, ASGN, BRANCH_COND, RETURN, CALL
+from expr import *
+
+def translate(self, ctx, insn, sp, end_bp, bp):
+
+  # normally, add() will follow the flow of the machine code instructions;
+  # if we know better (such as with branches with constant conditions), we
+  # can put a list of successor instructions here
+  next = None
+
+  st = SSAStatement()
+
+  debug(SSA, 2, 'translating', insn, hex(insn.bytes[0]), 'to', st.num)
+
+  opc = insn.bytes[0]
+
+  if insn == None:
+    return None
+  
+  st_start = st
+  self.ssa_for_insn[insn] = st
+  st.insn = insn
+
+  if len(insn.comefrom) > 1:
+    debug(SSA, 3, 'phiing')
+    for g in ctx.local_indices.values():
+      st.dest = [SSADef(ctx, g.type, g.addr)]
+      st.op = ASGN
+      st.expr = Expr(PHI, [g])
+      for h in insn.comefrom:
+        if h in self.last_ssa_for_insn:
+          #print('reaching from', self.last_ssa_for_insn[h], [str(x) + '(' + repr(x) + ')' for x in self.last_ssa_for_insn[h].reaching])
+          for i in self.last_ssa_for_insn[h].reaching:
+            if i.type == g.type and i.addr == g.addr and not i in st.expr.ops:
+              st.expr.ops += [i]
+      st1 = SSAStatement()
+      st.chain(ctx, st1)
+      st = st1
+  
+
+  def rot_imm(imm8, rs):
+    return ((imm8 >> rs * 2) | (imm8 << (32 - rs * 2))) & 0xffffffff
+
+  def do_ld_abs(reg, addr, size):
+    nonlocal st
+    st.op = ASGN
+    if self.is_io(addr):
+      # XXX: size?
+      if size == 1:
+        op = IOIN
+      elif size == 2:
+        op = IOIN16
+      else:
+        op = IOIN32
+      st.expr = Expr(op, [addr])
+      st.expr.dont_propagate = True
+      st.expr.dont_eliminate = True
+    else:
+      if size == 4:
+        st.expr = Expr(VAR, [SSADef.cur(ctx, 'Mw', addr)])
+      elif size == 2:
+        st.expr = Expr(VAR, [SSADef.cur(ctx, 'Mh', addr)])
+      elif size == 1:
+        st.expr = Expr(VAR, [SSADef.cur(ctx, 'M', addr)])
+      else:
+        raise InternalError('unhandled size ' + str(size))
+    st.dest = [SSADef(ctx, reg)]
+
+  
+  def do_ld_reg(dst, off, reg, size):
+    nonlocal st
+    st.op = ASGN
+    # XXX: convert to IOIN after constant prop
+    if size == 1:
+      st.expr = Expr(LOAD, [off, SSADef.cur(ctx, reg)])
+    elif size == 2:
+      st.expr = Expr(LOAD16, [off, SSADef.cur(ctx, reg)])
+    elif size == 4:
+      st.expr = Expr(LOAD32, [off, SSADef.cur(ctx, reg)])
+    else:
+      raise InternalError('unhandled size ' + str(size))
+    st.dest = [SSADef(ctx, dst)]
+
+  def do_st_abs(reg, addr, size):
+    if self.is_io(addr):
+      st.op = IMPURE
+      if size == 4:
+        op = IOOUT32
+      elif size == 2:
+        op = IOOUT16
+      else:
+        op = IOOUT
+      st.expr = Expr(op, [SSADef.cur(ctx, reg), addr])
+      st.expr.dont_propagate = True
+      st.expr.dont_eliminate = True
+      st.dest = []
+    else:
+      st.op = ASGN
+      st.expr = Expr(VAR, [SSADef.cur(ctx, reg)])
+      if size == 4:
+        pref = 'Mw'
+      elif size == 2:
+        pref = 'Mh'
+      elif size == 1:
+        pref = 'M'
+      else:
+        raise InternalError('unhandled size ' + str(size))
+      st.dest = [SSADef(ctx, pref, addr)]
+
+  def do_st_reg(src, off, reg, size):
+    st.dest = []
+    st.op = IMPURE
+    # XXX: convert to IOOUT after constant prop
+    if size == 4:
+      op = STORE32
+    elif size == 2:
+      op = STORE16
+    elif size == 1:
+      op = STORE
+    else:
+      raise InternalError('unhandled size ' + str(size))
+    st.expr = Expr(op, [SSADef.cur(ctx, src), off, SSADef.cur(ctx, reg)])
+
+  def do_ld_stack(dst, off, size):
+    nonlocal sp, st
+    st.op = ASGN
+    st.expr = Expr(VAR, [SSADef.cur(ctx, 's', off+sp)])
+    st.dest = [SSADef(ctx, dst)]
+  
+  def do_st_stack(src, off, size):
+    nonlocal sp, st
+    st.op = ASGN
+    st.expr = Expr(VAR, [SSADef.cur(ctx, src)])
+    st.dest = [SSADef(ctx, 's', off+sp)]
+
+  def get_rm():
+    if insn.shift == 0:
+      op = SHL
+    elif insn.shift == 1:
+      op = SHR
+    elif insn.shift == 2:
+      op = ASR
+    else:
+      op = ROR
+    if insn.shift_rot == 1:
+      # rs
+      return Expr(op, [SSADef.cur(ctx, 'R'+str(insn.rm)), SSADef.cur(ctx, 'R'+str(insn.rs))])
+    else:
+      # imm
+      return Expr(op, [SSADef.cur(ctx, 'R'+str(insn.rm)), insn.shift_bits])
+
+  if insn.cond < 0xe or insn.artificial_branch != -1:
+    debug(SSA, 5, "conditional", insn, hex(insn.artificial_branch))
+    # conditional
+    st.op = BRANCH_COND
+
+    if insn.cond == 0:
+      st.expr = Expr(VAR, [SSADef.cur(ctx, 'Z')])
+    elif insn.cond == 1:
+      st.expr = Expr(NOT, [SSADef.cur(ctx, 'Z')])
+    elif insn.cond == 2:
+      st.expr = Expr(VAR, [SSADef.cur(ctx, 'C')])
+    elif insn.cond == 3:
+      st.expr = Expr(NOT, [SSADef.cur(ctx, 'C')])
+    elif insn.cond == 8:
+      st.expr = Expr(AND, [SSADef.cur(ctx, 'C'), Expr(NOT, [SSADef.cur(ctx, 'Z')])])
+    elif insn.cond == 9:
+      st.expr = Expr(OR, [Expr(NOT, [SSADef.cur(ctx, 'C')]), SSADef.cur(ctx, 'Z')])
+    elif insn.cond == 0xa:
+      st.expr = Expr(COMPARE_EQ, [SSADef.cur(ctx, 'N'), SSADef.cur(ctx, 'V')])
+    elif insn.cond == 0xb:
+      st.expr = Expr(COMPARE_NE, [SSADef.cur(ctx, 'N'), SSADef.cur(ctx, 'V')])
+    elif insn.cond == 0xc:
+      st.expr = Expr(AND, [
+        Expr(NOT, [SSADef.cur(ctx, 'Z')]),
+        Expr(COMPARE_EQ, [SSADef.cur(ctx, 'N'), SSADef.cur(ctx, 'V')])
+      ])
+    elif insn.cond == 0xd:
+      st.expr = Expr(OR, [
+        SSADef.cur(ctx, 'Z'),
+        Expr(COMPARE_NE, [SSADef.cur(ctx, 'N'), SSADef.cur(ctx, 'V')])
+      ])
+    else:
+      raise InternalError('unsupported condition')
+
+    assert(insn.op & 0xf0 == 0xa0 or insn.artificial_branch != -1)
+
+  debug(SSA, 6, 'fuck insn', insn, insn.cond, insn.op)
+  if insn.cond == 0xf:	# NV predicate
+      debug(SSA, 5, "NV predicate")
+      st.dest = []
+      st.op = ASGN
+      st.expr = Expr(NOP, [0])
+  elif insn.op & 0xf0 == 0xa0:	# B
+      debug(SSA, 5, "branch")
+      if insn.cond == 0xe:
+        debug(SSA, 5, "unconditional branch")
+        st.op = ASGN
+        st.dest = []
+        st.expr = Expr(NOP, [0])
+  elif insn.op & 0xf1 == 0xe0:	# MCR
+      debug(SSA, 5, "mcr")
+      st.op = IMPURE
+      st.dest = []
+      st.expr = Expr(INTRINSIC, ['mcr'])
+  elif insn.op & 0xf1 == 0xe1:	# MRC
+      debug(SSA, 5, "mrc")
+      st.op = ASGN
+      st.dest = [SSADef(ctx, 'R'+str(insn.rd))]
+      st.expr = Expr(INTRINSIC, ['mrc'])
+  elif insn.op & 0xc0 == 0x40:	# LDR/STR rd,[rn,+-#imm12 / rm <> shift] pre/post
+      if insn.op & 0x20:
+        # register offset
+        if insn.op & 0x08 == 0x08:
+          off = get_rm()
+        else:
+          off = Expr(SUB, [0, get_rm()])
+      else:
+        # immediate offset
+        if insn.op & 0x08 == 0x08:
+          off = insn.imm12
+        else:
+          off = -insn.imm12
+      if insn.op & 0x04 == 0x04:
+        size = 1
+      else:
+        size = 4
+      if insn.fixed_mem != -1:
+        assert(insn.rn != 13)
+        debug(SSA, 6, 'transing fixed mem insn', insn)
+        if insn.op & 1:
+          do_ld_abs('R'+str(insn.rd), insn.fixed_mem, size)
+        else:
+          do_st_abs('R'+str(insn.rd), insn.fixed_mem, size)
+      else:
+        if insn.op & 1 == 1 and insn.rd == 15:
+          # PC load
+          if insn.rn == 15:
+            # decoded already in insn.py
+            st.dest = []
+            st.op = ASGN
+            st.expr = Expr(NOP, [0])
+          else:
+            raise InternalError("non-const PC load")
+        else:
+          if insn.op & 0x10:	# pre indexing
+            noff = off
+          else:
+            noff = 0
+          if insn.rn == 15:
+            if insn.op & 1:
+              do_ld_abs('R'+str(insn.rd), insn.addr + 8 + noff, size)
+            else:
+              do_st_abs('R'+str(insn.rd), insn.addr + 8 + noff, size)
+          elif insn.rn == 13:
+            if insn.op & 1:
+              do_ld_stack('R'+str(insn.rd), noff, size)
+            else:
+              do_st_stack('R'+str(insn.rd), noff, size)
+          else:
+            if insn.op & 1:
+              do_ld_reg('R'+str(insn.rd), noff, 'R'+str(insn.rn), size)
+            else:
+              do_st_reg('R'+str(insn.rd), noff, 'R'+str(insn.rn), size)
+      if insn.op & 2 == 2 or insn.op & 0x10 == 0:	# write back or post indexing
+        if insn.rn == 13:
+          sp += off
+        else:
+          st1 = SSAStatement()
+          st.chain(ctx, st1)
+          st = st1
+          st.op = ASGN
+          st.expr = Expr(ADD, [SSADef.cur(ctx, 'R'+str(insn.rn)), off])
+          st.dest = [SSADef(ctx, 'R'+str(insn.rn))]
+  elif insn.op & 0xe4 == 4 and insn.imm8 & 0x90 == 0x90:	# LDRH/STRH rd, [rn, +-immWEIRD]
+      imm = insn.rm | (insn.rs >> 4)
+      if insn.op & 0x08 == 0:
+        imm = -imm
+      if insn.fixed_mem != -1:
+        assert(insn.rn != 13)
+        debug(SSA, 6, 'transing fixed mem insn', insn)
+        if insn.op & 1:
+          do_ld_abs('R'+str(insn.rd), insn.fixed_mem, 2)
+        else:
+          do_st_abs('R'+str(insn.rd), insn.fixed_mem, 2)
+      else:
+        if insn.op & 0x10:	# pre indexing
+          if insn.op & 1:
+            do_ld_reg('R'+str(insn.rd), imm, 'R'+str(insn.rn), 2)
+          else:
+            do_st_reg('R'+str(insn.rd), imm, 'R'+str(insn.rn), 2)
+        else:
+          if insn.op & 1:
+            do_ld_reg('R'+str(insn.rd), 0, 'R'+str(insn.rn), 2)
+          else:
+            do_st_reg('R'+str(insn.rd), 0, 'R'+str(insn.rn), 2)
+      if insn.op & 2:	# write back
+        st1 = SSAStatement()
+        st.chain(ctx, st1)
+        st = st1
+        st.op = ASGN
+        st.expr = Expr(ADD, [SSADef.cur(ctx, 'R'+str(insn.rn)), imm])
+        st.dest = [SSADef(ctx, 'R'+str(insn.rn))]
+  elif insn.op == 0x32:	# MSR CPSR_f, rm
+      debug(SSA, 5, "MSR CPSR_f, rm")
+      st.op = IMPURE
+      st.dest = []
+      st.expr = Expr(INTRINSIC, ['msr_cpsr_f', SSADef.cur(ctx, 'R'+str(insn.rm))])
+  elif insn.op & 0xf0 == 0xb0:	# BL off24
+    st.expr = Expr(ARGS, [insn.addr + 8 + insn.off24 * 4] + self.fun_args(ctx, insn.next[1], st, sp))
+    st.dest = self.fun_returns(ctx, insn.next[1], st)
+    st.op = CALL
+  elif insn.op & 0xe0 == 0x80:	# LDM/STM
+    # XXX: PSR/force user?
+    off = 0
+    if insn.op & 0x08 == 0x08:
+      inc = 4
+      regs = range(0, 16)
+    else:
+      inc = -4
+      regs = range(15, -1, -1)
+    st.op = None
+    do_return = False
+    for i in regs:
+      if insn.reglist & (1 << i):
+        if insn.op & 0x10 == 0x10:	# pre indexing
+          off += inc
+        if st.op != None:
+          st1 = SSAStatement()
+          st.chain(ctx, st1)
+          st = st1
+        if insn.op & 1:	# LDM
+          if i == 15:
+            # XXX: only if [rn,off] is r14
+            do_return = True
+          elif insn.rn == 13:
+            do_ld_stack('R'+str(i), off, 4)
+          else:
+            do_ld_reg('R'+str(i), off, 'R'+str(insn.rn), 4)
+        else:
+          if insn.rn == 13:
+            do_st_stack('R'+str(i), off, 4)
+          else:
+            do_st_reg('R'+str(i), off, 'R'+str(insn.rn), 4)
+        if insn.op & 0x10 != 0x10:	# post indexing
+          off += inc
+    if insn.op & 2 == 2:	# write back
+        if insn.rn == 13:
+          sp += off
+          if end_bp == 0:
+            end_bp = sp
+        else:
+          if st.op != None:
+            st1 = SSAStatement()
+            st.chain(ctx, st1)
+            st = st1
+          st.op = ASGN
+          st.expr = Expr(ADD, [SSADef.cur(ctx, 'R'+str(insn.rn)), off])
+          st.dest = [SSADef(ctx, 'R'+str(insn.rn))]
+    if do_return:
+      if st.op != None:
+        st1 = SSAStatement()
+        st.chain(ctx, st1)
+        st = st1
+      st.op = RETURN
+      st.expr = None
+      st.dest = []
+  elif insn.bytes[0] & 0x0ffffff0 == 0x012fff10:	# BX rm
+    if insn.rm == 14:
+      # XXX: only if R14 is unassigned
+      st.dest = []
+      st.op = RETURN
+      st.expr = None
+    else:
+      st.dest = []
+      st.op = IMPURE
+      st.expr = Expr(INTRINSIC, ['bx', SSADef.cur(ctx, 'R'+str(insn.rm))])
+      st.add_comment('XXX: indirect jump not implemented yet')
+  elif insn.bytes[0] & 0xffffff0 == 0x012fff30:	# BLX rm
+    st.dest = []
+    st.op = IMPURE
+    st.expr = Expr(INTRINSIC, ['blx', SSADef.cur(ctx, 'R'+str(insn.rm))])
+    st.add_comment('XXX: indirect call not implemented yet')
+  elif insn.op & 0xc0 == 0x00:	# ALU rd, rn, #imm8 <> rs / rm <> shift
+      alu_op = (insn.op >> 1) & 0xf
+      debug(SSA, 5, "alu op", alu_op)
+      if insn.rd == 13 and alu_op in [2, 4]:
+        assert(insn.op & 0x20 == 0x20)	# immediate
+        if alu_op == 4:
+          # XXX: hack: assume conditional adds to SP only occur in epilogs and ignore them
+          # otherwise our stack pointer tracking gets mangled
+          if insn.cond == 0xe:
+            sp += rot_imm(insn.imm8, insn.rs)
+        else:
+          sp -= rot_imm(insn.imm8, insn.rs)
+        st.op = ASGN
+        st.dest = []
+        st.expr = Expr(NOP, [sp])
+      else:
+        st.op = None
+        if insn.op & 0x20 == 0:
+          imm = get_rm()
+        else:
+          imm = rot_imm(insn.imm8, insn.rs)
+
+        if alu_op in [2, 3, 6, 7, 0xa]:
+          op = SUB
+          op_c = COMPARE_GE
+          op_z = COMPARE_EQ
+          op_n = COMPARE_LT
+          op_v = SUBFLAGS_V
+        elif alu_op == 0 or alu_op == 8 or alu_op == 0xe:
+          op = AND
+          op_c = ANDFLAGS_C
+          op_z = ANDFLAGS_Z
+          op_n = ANDFLAGS_N
+          op_v = None
+        elif alu_op == 1 or alu_op == 9:
+          op = EOR
+          op_c = EORFLAGS_C
+          op_z = EORFLAGS_Z
+          op_n = EORFLAGS_N
+          op_v = None
+        elif alu_op == 4 or alu_op == 5 or alu_op == 0xb:
+          op = ADD
+          op_c = ADDFLAGS_C
+          op_z = ADDFLAGS_Z
+          op_n = ADDFLAGS_N
+          op_v = ADDFLAGS_V
+        elif alu_op == 0xc:
+          op = OR
+          op_c = ORFLAGS_C
+          op_z = ORFLAGS_Z
+          op_n = ORFLAGS_N
+          op_v = None
+        else:	# MOV, MVN
+          op = CONST
+          op_c = MOVFLAGS_C
+          op_z = MOVFLAGS_Z
+          op_n = MOVFLAGS_N
+          op_v = None
+
+        reverse = False
+        if alu_op == 3 or alu_op == 7:	# RSB, RSC
+          reverse = True
+
+        if alu_op == 0xf or alu_op == 0xe:	# BIC, MVN
+          imm = Expr(INV, [imm])
+
+        if op == CONST:
+          if insn.op & 0x20 == 0:
+            st.expr = imm
+          else:
+            st.expr = Expr(CONST, [imm])
+        else:
+          if insn.rn == 13 and insn.op & 0x20 == 0x20 and alu_op == 4:
+            st.expr = Expr(VAR, [SSADef.cur(ctx, 'ap', sp + imm)])
+            # XXX: may be better to do this on CALL
+            if sp + imm < bp:
+              bp = sp + imm
+          elif insn.rn == 13 and insn.op & 0x20 == 0x20 and alu_op == 2:
+            st.expr = Expr(VAR, [SSADef.cur(ctx, 'ap', sp - imm)])
+            if sp - imm < bp:
+              bp = sp - imm
+          else:
+            op1 = SSADef.cur(ctx, 'R'+str(insn.rn))
+            if reverse:
+              op2 = op1
+              op1 = imm
+            else:
+              op2 = imm
+
+            if alu_op == 5:	# ADC
+              st.expr = Expr(op, [op1, op2, SSADef.cur(ctx, 'C')])
+            elif alu_op == 6 or alu_op == 7:	# SBC, RSC
+              st.expr = Expr(ADD, [Expr(op, [op1, op2, 1]), SSADef.cur(ctx, 'C')])
+            else:
+              st.expr = Expr(op, [op1, op2])
+
+        if alu_op in [0, 1, 2, 3, 4, 5, 6, 7, 0xc, 0xd, 0xe, 0xf] and insn.rd != 15:
+          st.op = ASGN
+          st.dest = [SSADef(ctx, 'R'+str(insn.rd))]
+        if insn.rd == 15:
+          st.op = IMPURE
+          st.expr = Expr(INTRINSIC, ['set_pc', st.expr])
+          st.dest = []
+
+        if insn.op & 1:	# set flags
+          alu_expr = st.expr
+          if st.op == ASGN:
+            st1 = SSAStatement()
+            st.chain(ctx, st1)
+            st = st1
+          st.op = ASGN
+          st.expr = Expr(op_c, copy(alu_expr.ops))
+          st.dest = [SSADef(ctx, 'C')]
+          st1 = SSAStatement()
+          st.chain(ctx, st1)
+          st = st1
+          st.op = ASGN
+          st.expr = Expr(op_z, copy(alu_expr.ops))
+          st.dest = [SSADef(ctx, 'Z')]
+          st1 = SSAStatement()
+          st.chain(ctx, st1)
+          st = st1
+          st.op = ASGN
+          st.expr = Expr(op_n, copy(alu_expr.ops))
+          st.dest = [SSADef(ctx, 'N')]
+          if op_v:
+            st1 = SSAStatement()
+            st.chain(ctx, st1)
+            st = st1
+            st.op = ASGN
+            st.expr = Expr(op_v, copy(alu_expr.ops))
+            st.dest = [SSADef(ctx, 'V')]
+          
+        
+  else:
+    raise InternalError('unknown op ' + hex(insn.op))
+
+  # all current indices are reaching, with the exception of anything
+  # below the stack pointer
+  st.reaching = []
+  for i in ctx.local_indices.values():
+    if not (i.type == 's' and i.addr < 0): #True: #i.type != 's':# or i.addr > sp:
+      st.reaching += [i]
+
+  self.last_ssa_for_insn[insn] = st
+  debug(SSA, 6, "st_end in translate", st)
+  return (st_start, st, sp, bp, end_bp, next)
